e9664d918a2d6d193f6fad0f718301a4dcd92424
k1r0s <ciroreed@gmail.com>
2017-08-27 02:58:07 +0200

    php script handles 404 on wrong resource name

--- back-php/src/api.php
+++ back-php/src/api.php
@@ -1,6 +1,19 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+$resourceList = array("session", "messages", "profiles");
+
 $method = $_SERVER["REQUEST_METHOD"];
-$uri = $_SERVER["REQUEST_URI"];
+$path = explode("/", $_SERVER["REQUEST_URI"]);
 $query = $_SERVER["QUERY_STRING"];
 $data = file_get_contents('php://input');
+$resourceName = $path[1];
+$resourceId = $path[2];
+
+if(!in_array($resourceName, $resourceList)) {
+  http_response_code(404);
+  exit();
+}
+
+http_response_code(200);
+exit();
